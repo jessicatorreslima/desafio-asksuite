5a815851dda3e2de8e2ff1d594d6f2421621fe10
Jéssica Torres <jtlimapro@gmail.com>
2023-04-02 21:37:00 -0300

    Pequenos ajustes no route.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const RoomService = require("../services/RoomService");
+const express = require('express');
+const RoomService = require('../services/RoomService');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello Asksuite World!");
+router.get('/', (req, res) => {
+    res.send('Hello Asksuite World!');
 });
 
-router.post("/search", async (req, res) => {
+router.post('/search', async (req, res) => {
     const { checkin, checkout } = req.body;
 
-    const errors = validateParams(checkin, checkout);
-    if (errors.length > 0) {
-        return res.status(400).json({ errors });
+    const validationErrors = validateParams(checkin, checkout);
+    if (validationErrors.length > 0) {
+        return res.status(400).json({ error: validationErrors });
     }
 
     try {
         const rooms = await RoomService.getRooms(checkin, checkout);
-        res.status(200).json(rooms);
+        return res.status(200).json(rooms);
     } catch (error) {
-        res.status(error.status || 500).json({ error: error.message });
+        return res.status(error.status || 500).json({ error: error.message });
     }
 });
 
-router.all("**", (req, res) => {
+router.all('**', (req, res) => {
     res.status(403);
-    res.send("Not alowed!");
+    res.send('Not alowed!');
 });
 
 function validateParams(checkin, checkout) {
@@ -39,14 +39,14 @@ function validateParams(checkin, checkout) {
         }
     }
 
-    validateDate(checkin, 'Check-in');
-    validateDate(checkout, 'Checkout');
+    validateDate(checkin, 'checkin');
+    validateDate(checkout, 'checkout');
 
     return Object.values(errors);
 }
 
 function isValidDate(dateString) {
-    let pattern = /^\d{4}-\d{2}-\d{2}$/;
+    const pattern = /^\d{4}-\d{2}-\d{2}$/;
     return pattern.test(dateString);
 }
 
